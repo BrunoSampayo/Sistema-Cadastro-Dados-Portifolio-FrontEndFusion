e6ae8fd408c49513e879e7020bd93572ff5a047e
Bruno <brunosampayo1040@gmail.com>
2024-05-31 16:26:05 -0300

    fix team getData

--- src/app/(team)/team/page.tsx
+++ src/app/(team)/team/page.tsx
@@ -5,10 +5,15 @@ import { DataTable } from './_components/table/data-table'
 import AddTeamForm from './_components/addTeamForm'
 
 export default async function TeamPage() {
-  const teams = await prismadb.team.findMany({
-    where: {},
-  })
+  const getData = async () => {
+    'use server'
+    const teams = await prismadb.team.findMany({
+      where: {},
+    })
+    return teams
+  }
 
+  const data = await getData()
   return (
     <div className="text-white container space-y-6">
       <h1 className="text-center text-2xl mb-4"> </h1>
@@ -16,7 +21,7 @@ export default async function TeamPage() {
         <AddTeamForm />
       </div>
 
-      <DataTable columns={columns} data={teams} />
+      <DataTable columns={columns} data={data} />
     </div>
   )
 }
